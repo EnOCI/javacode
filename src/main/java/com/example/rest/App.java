1d57d743e9d8257395366a23fae59fcc4ee7d2b6
EnOCI <39405647+EnOCI@users.noreply.github.com>
2018-05-18 15:09:40 +0200

    Update App.java

--- src/main/java/com/example/rest/App.java
+++ src/main/java/com/example/rest/App.java
@@ -57,7 +57,7 @@ public class App {
  
     	// sample URL
      //String url = "http://ip.jsontest.com/";     
-     String url = "http://129.157.179.180:3000/reactorCore/33/"+y+"/White/EnOCI";
+     String url = "http://129.157.179.180:3000/fighters/33/"+y+"/White/EnOCI";
      
         CloseableHttpResponse response = null;
  
